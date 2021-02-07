fd98cb3d452f2c61119c18ee18c241da6c07b044
Azambik <71612017+Azambik@users.noreply.github.com>
2021-02-06 21:58:14 -0500

    finished routs

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,14 @@
-//import block --
 const Sequelize = require('sequelize');
-//used to hide critical data
-require('dotenv').config();
-//import block --
 
-//sequelize connection settings
+require('dotenv').config();
+
+// create connection to our db
 const sequelize = process.env.JAWSDB_URL
-? new Sequelize(process.env.JAWSDB_URL)
-: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
 module.exports = sequelize;
